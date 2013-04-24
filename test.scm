c553243b3ee3b6df893560782f775bffc6a6d042
Tim Shen <innocentsty@gmail.com>
2013-04-24 17:48:19 +0800

    README and test file

--- test.scm
+++ test.scm
@@ -1,15 +1,18 @@
-;(define a 1)
-;a
-;
-;(define-syntax my
-;  (syntax-rules ()
-;                ((y x) x)))
-;(my 3)
-;
-;(define (a) (+ 3 2))
-;(a)
 (define (fib x)
-  (if (= x 0) 0
-              (if (= x 1) 1
-                          (+ (fib (- x 1)) (fib (- x 2))))))
-(fib 20)
+  (if (= x 0)
+      0
+      (if (= x 1)
+          1
+          (+ (fib (- x 1)) (fib (- x 2))))))
+(fib 1)
+(fib 2)
+(fib 3)
+(fib 4)
+(fib 5)
+(fib 6)
+(fib 7)
+(fib 8)
+(fib 9)
+(fib 11)
+(fib 12)
+(fib 13)
